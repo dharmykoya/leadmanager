33e83cc97d389588e4660308168c5a22266745c7
Damilola Adekoya <dharmykoya38@gmail.com>
2019-09-08 00:46:58 +0100

    feature(setup-redux):setup redux To setup redux To connect with backend for getting all leads To connect with backend for deleting a lead To connect eith backend for creating lead

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+
+//GET_LEADS
+export const getLeads = () => dispatch => {
+  axios.get('/api/leads/').then(res => {
+    dispatch({
+      type: GET_LEADS,
+      payload: res.data
+    }).catch(error => console.log(12, error));
+  });
+};
+
+//DELETE LEAD
+export const deleteLead = id => dispatch => {
+  axios.delete(`/api/leads/${id}/`).then(res => {
+    dispatch({
+      type: DELETE_LEAD,
+      payload: id
+    }).catch(error => console.log(12, error));
+  });
+};
+
+//ADD LEAD
+export const addLead = lead => dispatch => {
+  axios.post('/api/leads/', lead).then(res => {
+    dispatch({
+      type: ADD_LEAD,
+      payload: res.data
+    }).catch(error => console.log(12, error));
+  });
+};
--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 
+import { Provider } from 'react-redux';
+import store from '../store';
+
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <div className="container">
-          <h1>React App with django by Damilola Adekoya</h1>
-          <Dashboard />
-        </div>
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <h1>React App with django by Damilola Adekoya</h1>
+            <Dashboard />
+          </div>
+        </Fragment>
+      </Provider>
     );
   }
 }
