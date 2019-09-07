adfcfd9c4532ba666bf987594054fbeeffaa12dd
Damilola Adekoya <dharmykoya38@gmail.com>
2019-09-07 16:21:06 +0100

    first commit

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <h1>React App with django by Damilola Adekoya</h1>
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
